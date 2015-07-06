fde610c68db5c010dc40bcc89b7b26cff447ae27
katano <katano@sample.com>
2015-07-06 11:58:58 +0900

    katano hover 2nd

--- warako/js/menuHover.js
+++ warako/js/menuHover.js
@@ -1,7 +1,7 @@
 $(function(){
 	$('.nav ul li a').hover(function() {
-		$(this).stop(false, false).animate({backgroundColor: "#ffd200", color:"#000000"}, 500);
+		$(this).stop(false, true).animate({backgroundColor: "#ffffff", color:"#4682b4"}, 500);
 	},function() {
-		$(this).stop(false, false).animate({ backgroundColor: "#000000", color:"#cccccc"}, 500);
+		$(this).stop(false, true).animate({ backgroundColor: "#4682b4", color:"#ffffff"}, 500);
 	});
 });
